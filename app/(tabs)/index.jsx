be8029291468d3de42a258834cd46bcb0d35ce66
Aditya Kurne <137620112+EMPER0R7@users.noreply.github.com>
2024-09-30 13:15:09 +0530

    dsh

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,7 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View ,ActivityIndicator } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import  { useEffect, useState } from 'react';
+
+import { auth } from '../../src/screen/firebase'; // Firebase Auth import
 
 import Homescreen from '../../src/screen/Homescreen.jsx';
 import LoginScreen from '../../src/screen/LoginScreen.jsx';
@@ -14,8 +17,25 @@ const Stack = createNativeStackNavigator();
 
 
 const App = () => {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const subscriber = auth.onAuthStateChanged((user) => {
+      setUser(user);
+      if (initializing) setInitializing(false);
+    });
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return <ActivityIndicator size="large" />;
+
   return (
     <NavigationContainer independent={true}>
+      {user ? (
+          <Stack.Screen name="Dashboard" component={Dashboard} />
+        ) : (
+          <Stack.Screen name="Home" component={Homescreen} />
+        )}
       <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Home" component={Homescreen} />
       <Stack.Screen name="LOGIN" component={LoginScreen} />
--- src/screen/Dashboard.jsx
+++ src/screen/Dashboard.jsx
@@ -1,24 +1,88 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,TouchableOpacity} from 'react-native'
 import React from 'react'
-const firebaseConfig = {
-    apiKey: "AIzaSyCDNXN1OP6Z12vMOivlS_uNUwKw-S7XEkk",
-    authDomain: "gharbuddies.firebaseapp.com",
-    projectId: "gharbuddies",
-    storageBucket: "gharbuddies.appspot.com",
-    messagingSenderId: "308634445349",
-    appId: "1:308634445349:web:0e90c39f4a68a6b152c1cc",
-    measurementId: "G-H5VRYLW21V"
-  };
+import colors, { colours } from '../utils/colours.js'
+import { fonts } from '../utils/fonts';
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCDNXN1OP6Z12vMOivlS_uNUwKw-S7XEkk",
+//     authDomain: "gharbuddies.firebaseapp.com",
+//     projectId: "gharbuddies",
+//     storageBucket: "gharbuddies.appspot.com",
+//     messagingSenderId: "308634445349",
+//     appId: "1:308634445349:web:0e90c39f4a68a6b152c1cc",
+//     measurementId: "G-H5VRYLW21V"
+//   };
   
 
 const Dashboard = () => {
   return (
-    <View>
-      <Text>Dashboard</Text>
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={[
+            styles.loginButtonWrapper,
+            { backgroundColor: colours.primary },
+          ]}
+          
+        >
+          <Text style={styles.loginButtonText}>INVEST</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.loginButtonWrapper]}
+          
+        >
+          <Text style={styles.signupButtonText}>RENT</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 
 export default Dashboard
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor:colours.white,
+},
+
+text:{
+    fontSize:25,
+    fontFamily:fonts.Bold,
+    textAlign: 'center',
+    paddingTop: 50,
+    fontStyle:"italic",
+    fontWeight:"bold",
+    
+
+},
+suntext:{
+    textAlign:'center',
+    marginTop:50,
+    
+},
+buttonContainer: {
+    marginTop: 400,
+    flexDirection: "row",
+    borderWidth: 2,
+    borderColor: colours.primary,
+    width: "70%",
+    height: 60,
+    borderRadius: 100,
+    alignSelf:"center"},
+  loginButtonWrapper: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "50%",
+    borderRadius: 98,
+  },
+  loginButtonText: {
+    color: colours.white,
+    fontSize: 18,
+    fontFamily:fonts.Light,
+  },
+  signupButtonText: {
+    fontSize: 18,
+    
+  },
+
+})
